4c6426f6d42f9c2489e6338102e9f57e444a8871
Jeff Neet <jeff.neet@gmail.com>
2020-10-03 12:22:43 -0600

    Step 4 - log button click

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          child: Image.asset('images/ball1.png'),
+          onPressed: () {
+            print('I got clicked');
+          },
+        ),
+      ),
+    );
+  }
+}
